5da29e86c067d3f326d532f5afb3c1b4c80b5091
DaBoss120 <164732130+DaBoss120@users.noreply.github.com>
2024-06-30 16:01:19 +0200

    Elements added and updated
    - Added a sub-navigation for LE CLUB.
    - Updated text in carousel images to make it appear better on resize

--- WEB/JS/script.js
+++ WEB/JS/script.js
@@ -3,26 +3,40 @@
 let slideIndex = 1;
 showSlides(slideIndex)
 
-function plusSlides(nb){
+document.querySelector('.LeClub').addEventListener('mouseover', event => {
+    document.querySelector('.subnav-content').classList.add('show');
+});
+document.querySelector('.LeClub').addEventListener('mouseout', event => {
+
+    setTimeout(() => {
+        if (!document.querySelector('.LeClub').matches(':hover')) {
+            document.querySelector('.subnav-content').classList.remove('show')
+        }
+    }, 1000);
+
+});
+
+
+function plusSlides(nb) {
     showSlides(slideIndex += nb);
 }
 
-function currentSlide(nb){
+function currentSlide(nb) {
     showSlides(slideIndex = nb);
 }
 
-function showSlides(nb){
+function showSlides(nb) {
     let i;
     let slides = document.getElementsByClassName("carouselSlides");
     let dots = document.getElementsByClassName("dot");
-    if(nb > slides.length ){slideIndex = 1};
-    if(nb < 1){slideIndex = slides.length}
-    for(i = 0; i < slides.length; i++){
+    if (nb > slides.length) { slideIndex = 1 };
+    if (nb < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for(i = 0; i< dots.length; i++){
+    for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
