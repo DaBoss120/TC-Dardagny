261775d4e2c4f215314920b26364a804dec949bc
DaBoss120 <164732130+DaBoss120@users.noreply.github.com>
2024-07-03 17:35:18 +0200

    Created Cours de tennis page
    - Created coursDeTennis.html
    - [new] Teachers are presented the same way that the committee persons are; with a nice background blur on an image with neat animations on hover

--- WEB/JS/script.js
+++ WEB/JS/script.js
@@ -52,6 +52,56 @@ document.querySelectorAll('.personnes-comite').forEach(element => {
         // }, 200);
     })
 });
+
+document.querySelectorAll('.personnes-professeurs').forEach(element => {
+    element.addEventListener('mouseover', event => {
+        let par = event.target.querySelector('.diplomes-professeurs');
+
+        // let par = document.querySelector('.personnes-comite p');
+        par.classList.add('showPar');
+        event.target.querySelectorAll('p').forEach(element => {
+            element.classList.add('showPar');
+        });
+        setTimeout(() => {
+            par.classList.add('unBlur');
+            event.target.querySelectorAll('p').forEach(element => {
+                element.classList.add('unBlur');
+            })
+
+        }, 1);
+
+        // par.style.display = "block";
+        // par.style.filter = "blur(0px)";
+        // par.style.transition = "0.3s";
+    })
+});
+document.querySelectorAll('.personnes-professeurs').forEach(element => {
+    element.addEventListener('mouseout', event => {
+
+        let par = event.target.querySelector('.diplomes-professeurs');
+        // par.style.display = 'none';
+        if (!element.matches(':hover')) {
+            setTimeout(() => {
+                if (!element.matches(':hover')) {
+                    par.classList.remove('showPar');
+                    event.target.querySelectorAll('p').forEach(element => {
+                        element.classList.remove('showPar');
+                    });
+                }
+            }, 250)
+            par.classList.remove('unBlur');
+            event.target.querySelectorAll('p').forEach(element => {
+                element.classList.remove('unBlur');
+            });
+
+        }
+        // setTimeout(()=>{
+        //     document.querySelector('.personnes-comite p').style.display = "none";
+        // }, 200);
+    })
+});
+
+
 // function plusSlides(nb) {
 //     showSlides(slideIndex += nb);
 // }
