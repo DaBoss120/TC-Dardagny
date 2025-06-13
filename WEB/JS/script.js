31108fc7576141e8cf7bb9a754be1f4d8c06b8dd
DaBoss120 <164732130+DaBoss120@users.noreply.github.com>
2025-06-13 19:31:14 +0200

    Code refactoring
    - [Refactoring] : Created a new CSS file for each page, refactored and organized code.
    - [Edit] : Made the removal of the show class for the blurred paragraph in comite 300ms instead of 250ms

--- WEB/JS/script.js
+++ WEB/JS/script.js
@@ -18,38 +18,23 @@ document.querySelector('.LeClub').addEventListener('mouseout', event => {
 
 document.querySelectorAll('.personnes-comite').forEach(element => {
     element.addEventListener('mouseover', event => {
-        let par = event.target.querySelector('p');
-
-        // let par = document.querySelector('.personnes-comite p');
         event.target.querySelector('p').classList.add('showPar');
         setTimeout(() => {
             event.target.querySelector('p').classList.add('unBlur');
-
-        }, 1);
-
-        // par.style.display = "block";
-        // par.style.filter = "blur(0px)";
-        // par.style.transition = "0.3s";
+        }, 0);
     })
 });
 document.querySelectorAll('.personnes-comite').forEach(element => {
     element.addEventListener('mouseout', event => {
-
-        let par = event.target.querySelector('p');
-        // par.style.display = 'none';
         if (!element.matches(':hover')) {
             setTimeout(() => {
                 if (!element.matches(':hover')) {
 
                     event.target.querySelector('p').classList.remove('showPar');
                 }
-            }, 250)
+            }, 300)
             event.target.querySelector('p').classList.remove('unBlur');
-
         }
-        // setTimeout(()=>{
-        //     document.querySelector('.personnes-comite p').style.display = "none";
-        // }, 200);
     })
 });
 
@@ -68,11 +53,7 @@ document.querySelectorAll('.personnes-professeurs').forEach(element => {
                 element.classList.add('unBlur');
             })
 
-        }, 1);
-
-        // par.style.display = "block";
-        // par.style.filter = "blur(0px)";
-        // par.style.transition = "0.3s";
+        }, 0);
     })
 });
 document.querySelectorAll('.personnes-professeurs').forEach(element => {
@@ -95,9 +76,6 @@ document.querySelectorAll('.personnes-professeurs').forEach(element => {
             });
 
         }
-        // setTimeout(()=>{
-        //     document.querySelector('.personnes-comite p').style.display = "none";
-        // }, 200);
     })
 });
 // Button of class 1
@@ -135,27 +113,3 @@ document.querySelectorAll('.button2 button').forEach(element => {
     }
     )
 });
-
-// function plusSlides(nb) {
-//     showSlides(slideIndex += nb);
-// }
-
-// function currentSlide(nb) {
-//     showSlides(slideIndex = nb);
-// }
-
-// function showSlides(nb) {
-//     let i;
-//     let slides = document.getElementsByClassName("carouselSlides");
-//     let dots = document.getElementsByClassName("dot");
-//     if (nb > slides.length) { slideIndex = 1 };
-//     if (nb < 1) { slideIndex = slides.length }
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//     slides[slideIndex - 1].style.display = "block";
-//     dots[slideIndex - 1].className += " active";
-// }
