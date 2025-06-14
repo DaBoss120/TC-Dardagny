a7d1994702bae989a3c37316c55af254dc22a582
DaBoss120 <164732130+DaBoss120@users.noreply.github.com>
2025-06-14 15:52:07 +0200

    Blur cards full css
    - [Optimization] : Turned the cards with blur text appearing using only CSS.

--- WEB/JS/script.js
+++ WEB/JS/script.js
@@ -61,68 +61,68 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 });
 
-document.querySelectorAll('.personnes-comite').forEach(element => {
-    element.addEventListener('mouseover', event => {
-        event.target.querySelector('p').classList.add('showPar');
-        setTimeout(() => {
-            event.target.querySelector('p').classList.add('unBlur');
-        }, 0);
-    })
-});
-document.querySelectorAll('.personnes-comite').forEach(element => {
-    element.addEventListener('mouseout', event => {
-        if (!element.matches(':hover')) {
-            setTimeout(() => {
-                if (!element.matches(':hover')) {
+// document.querySelectorAll('.personnes-comite').forEach(element => {
+//     element.addEventListener('mouseover', event => {
+//         event.target.querySelector('p').classList.add('showPar');
+//         setTimeout(() => {
+//             event.target.querySelector('p').classList.add('unBlur');
+//         }, 0);
+//     })
+// });
+// document.querySelectorAll('.personnes-comite').forEach(element => {
+//     element.addEventListener('mouseout', event => {
+//         if (!element.matches(':hover')) {
+//             setTimeout(() => {
+//                 if (!element.matches(':hover')) {
 
-                    event.target.querySelector('p').classList.remove('showPar');
-                }
-            }, 300)
-            event.target.querySelector('p').classList.remove('unBlur');
-        }
-    })
-});
+//                     event.target.querySelector('p').classList.remove('showPar');
+//                 }
+//             }, 300)
+//             event.target.querySelector('p').classList.remove('unBlur');
+//         }
+//     })
+// });
 
-document.querySelectorAll('.personnes-professeurs').forEach(element => {
-    element.addEventListener('mouseover', event => {
-        let par = event.target.querySelector('.diplomes-professeurs');
+// document.querySelectorAll('.personnes-professeurs').forEach(element => {
+//     element.addEventListener('mouseover', event => {
+//         let par = event.target.querySelector('.diplomes-professeurs');
 
-        // let par = document.querySelector('.personnes-comite p');
-        par.classList.add('showPar');
-        event.target.querySelectorAll('p').forEach(element => {
-            element.classList.add('showPar');
-        });
-        setTimeout(() => {
-            par.classList.add('unBlur');
-            event.target.querySelectorAll('p').forEach(element => {
-                element.classList.add('unBlur');
-            })
+//         // let par = document.querySelector('.personnes-comite p');
+//         par.classList.add('showPar');
+//         event.target.querySelectorAll('p').forEach(element => {
+//             element.classList.add('showPar');
+//         });
+//         setTimeout(() => {
+//             par.classList.add('unBlur');
+//             event.target.querySelectorAll('p').forEach(element => {
+//                 element.classList.add('unBlur');
+//             })
 
-        }, 0);
-    })
-});
-document.querySelectorAll('.personnes-professeurs').forEach(element => {
-    element.addEventListener('mouseout', event => {
+//         }, 0);
+//     })
+// });
+// document.querySelectorAll('.personnes-professeurs').forEach(element => {
+//     element.addEventListener('mouseout', event => {
 
-        let par = event.target.querySelector('.diplomes-professeurs');
-        // par.style.display = 'none';
-        if (!element.matches(':hover')) {
-            setTimeout(() => {
-                if (!element.matches(':hover')) {
-                    par.classList.remove('showPar');
-                    event.target.querySelectorAll('p').forEach(element => {
-                        element.classList.remove('showPar');
-                    });
-                }
-            }, 250)
-            par.classList.remove('unBlur');
-            event.target.querySelectorAll('p').forEach(element => {
-                element.classList.remove('unBlur');
-            });
+//         let par = event.target.querySelector('.diplomes-professeurs');
+//         // par.style.display = 'none';
+//         if (!element.matches(':hover')) {
+//             setTimeout(() => {
+//                 if (!element.matches(':hover')) {
+//                     par.classList.remove('showPar');
+//                     event.target.querySelectorAll('p').forEach(element => {
+//                         element.classList.remove('showPar');
+//                     });
+//                 }
+//             }, 250)
+//             par.classList.remove('unBlur');
+//             event.target.querySelectorAll('p').forEach(element => {
+//                 element.classList.remove('unBlur');
+//             });
 
-        }
-    })
-});
+//         }
+//     })
+// });
 // Button of class 1
 document.querySelectorAll('.button1 button').forEach(element => {
     element.addEventListener('mouseover', event => {
